96a6020e05525c42e990970514cacc7cfcfd1af8
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-11-11 21:37:17 -0300

    IdiomaSistema

---
+++ IdiomaSistema/src/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idiomasistema;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author marco
+ */
+public class IdiomaSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Locale idioma = Locale.getDefault();
+        System.out.print("Seu sistema está em ");
+        System.out.println(idioma.getDisplayLanguage());
+    }
+}
